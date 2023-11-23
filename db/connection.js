371011440b14b99f3f1c1272a097fc783a7dc2e0
TomMirfin <t.a.mirfin@gmail.com>
2023-11-23 13:56:18 +0000

    setting up prod DB

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,17 @@ const { Pool } = require("pg");
 
 const db = new Pool();
 const ENV = process.env.NODE_ENV || "development";
-
+const config = {};
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
 module.exports = db;
