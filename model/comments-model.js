5268269ee5cd8b449b76e57dd9550a05cb8e88b7
TomMirfin <t.a.mirfin@gmail.com>
2023-11-23 14:52:44 +0000

    added require

--- model/comments-model.js
+++ model/comments-model.js
@@ -1,5 +1,17 @@
 const db = require("../db/connection.js");
 
+exports.postComment = ({ username, body }, article_id) => {
+  return db
+    .query(
+      `INSERT INTO comments (author, body, article_id)
+    VALUES ($1, $2, $3) RETURNING *`,
+      [username, body, article_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
+
 exports.selectComments = (article_id) => {
   return db
     .query("SELECT * FROM comments WHERE article_id = $1;", [article_id])
